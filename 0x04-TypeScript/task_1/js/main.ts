413a4d0715245aca5052197dc1afae245e2aad5f
Emmanuel <emmanuelmark501@gmail.com>
2025-11-14 14:35:27 -0800

    writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,22 @@ function printTeacher({
 // const printTeacher: printTeacherFunction = (firstName, lastName) => {
 //   return `${firstName}. ${lastName}`;
 // };
+
+// Writing a class
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
