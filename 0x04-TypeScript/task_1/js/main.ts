146defb0829b3bf5aadeaf9f0c52d50f12bd4d68
Emmanuel <emmanuelmark501@gmail.com>
2025-11-14 13:41:21 -0800

    writing an interface for a function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,16 @@ interface Teacher {
   [key: string]: any; // allowance for any additional properties
 }
 
+// Extending the Teacher interface to create a Director interface
 interface Director extends Teacher {
   numberOfReports: number;
 }
+
+// Function interface for printing teacher's name
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+// Implementation of the printTeacher function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName}. ${lastName}`;
+};
